ae9b621b471b1702f2285d1a5e72a1fa2c48c035
Gijs Lamon <Gileba@users.noreply.github.com>
2017-05-29 09:10:36 +0200

    Use JLess for every change (#18)
    * First attempt on using JLess onBeforeRender
    * Code optimalisations after merge of Allrounder / Less Compiler
        * client is not needed in compileLess
        * remove duplicated and/or unnecessary code
        * fill table with template parameters
    * version bump

--- lesstemplatecompanion.php
+++ lesstemplatecompanion.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		System Plugin - Less Template Companion, an automatic Less compiler for developers and users
- * @version		0.1.0-alpha.4
+ * @version		0.1.0-alpha.5
  * @author		Gijs Lamon
  * @copyright	(C) 2017 Gijs Lamon
  * @license		GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -74,12 +74,22 @@ class plgSystemLessTemplateCompanion extends JPlugin
 	function onBeforeRender()
 	{
 		//path to less file
-		$lessFile = '';
+		$lessFile 	= '';
+		$table		= $this->app->getTemplate(true);
 
 		// 0 = frontend only
 		// 1 = backend only
 		// 2 = front + backend
 		$mode = $this->params->get('mode', 0);
+
+		// Convert the template params to an object.
+		// TODO: load template params
+		if (is_string($table->params))
+		{
+			$registry = new \Joomla\Registry\Registry;
+			$registry->loadString($table->params);
+			$table->params = $registry;
+		}
 
 		//only execute frontend
 		if ($this->app->isSite() && ($mode == 0 || $mode == 2))
@@ -119,7 +129,7 @@ class plgSystemLessTemplateCompanion extends JPlugin
 			//initialise less compiler
 			try
 			{
-				$this->autoCompileLess($lessFile, $cssFile);
+				$this->compileLess($table, $templatePath, $lessFile, $cssFile);
 			}
 			catch (Exception $e)
 			{
@@ -128,75 +138,6 @@ class plgSystemLessTemplateCompanion extends JPlugin
 		}
 
 		return false;
-	}
-
-	/**
-	 * Checks if .less file has been updated and stores it in cache for quick comparison.
-	 *
-	 * This function is taken and modified from documentation of lessphp
-	 *
-	 * @param String $inputFile
-	 * @param String $outputFile
-	 */
-	function autoCompileLess($inputFile, $outputFile)
-	{
-		// load config file
-		$config = JFactory::getConfig();
-		//path to temp folder
-		$tmpPath = $config->get('tmp_path');
-
-		//load chached file
-		$cacheFile = $tmpPath . DIRECTORY_SEPARATOR . $this->app->getTemplate() . "_" . basename($inputFile) . ".cache";
-
-		if (file_exists($cacheFile))
-		{
-			$tmpCache = unserialize(file_get_contents($cacheFile));
-			if ($tmpCache['root'] === $inputFile)
-			{
-				$cache = $tmpCache;
-			}
-			else
-			{
-				$cache = $inputFile;
-				unlink($cacheFile);
-			}
-		}
-		else
-		{
-			$cache = $inputFile;
-		}
-
-		//instantiate less compiler
-		$less = new lessc;
-
-		//set less options
-		//option: force recompilation regardless of change
-		$force = (boolean) $this->params->get('less_force', 0);
-
-		//option: preserve comments
-		if ($this->params->get('less_comments', 0))
-		{
-			$less->setPreserveComments(true);
-		}
-
-		//option: compression
-		if ($this->params->get('less_compress', 0))
-		{
-			$less->setFormatter("compressed");
-		}
-		else
-		{
-			$less->setFormatter("classic");
-		}
-
-		//compile cache file
-		$newCache = $less->cachedCompile($cache, $force);
-
-		if (!is_array($cache) || $newCache["updated"] > $cache["updated"])
-		{
-			file_put_contents($cacheFile, serialize($newCache));
-			file_put_contents($outputFile, $newCache['compiled']);
-		}
 	}
 
 	/**
@@ -395,11 +336,11 @@ class plgSystemLessTemplateCompanion extends JPlugin
 		// Check if .less file exists and is readable
 		if (is_readable($lessFile))
 		{
-			$this->compileLess($table, $client, $templatePath, $lessFile, $cssFile);
+			$this->compileLess($table, $templatePath, $lessFile, $cssFile);
 		}
 	}
 	
-	public function compileLess($table, $client, $templatePath, $lessFile, $cssFile)
+	public function compileLess($table, $templatePath, $lessFile, $cssFile)
 	{
 		$less = new JLess;
 
@@ -450,10 +391,6 @@ class plgSystemLessTemplateCompanion extends JPlugin
 				$value = '""';
 			}
 		}
-
-		// Adding template path to params
-		$basePath                 = ($table->client_id) ? JURI::base(true) : JURI::root(true);
-		$params_array['basePath'] = '"' . $basePath . '/"';
 
 		$less->setVariables($params_array);
 
