a64306b8698ca4c1094a1a872fa11a4af0a280fe
Gileba <info@gileba.be>
2017-06-04 23:56:57 +0200

    Test Class

--- tests/unit/setlessvariablesTest.php
+++ tests/unit/setlessvariablesTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 require_once('./lesstemplatecompanion.php');
 
-class plgSystemLessTemplateCompanion extends TestCase
+class plgSystemLessTemplateCompanionTest extends TestCase
 {
     public function testSetLessVariables()
     {
