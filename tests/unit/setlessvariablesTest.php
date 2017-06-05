9d908f002024fba0c6aafbb1fbdb0e2574f2eb82
Gileba <info@gileba.be>
2017-06-05 11:24:19 +0200

    new try

--- tests/unit/setlessvariablesTest.php
+++ tests/unit/setlessvariablesTest.php
@@ -5,15 +5,16 @@ require_once('./lesstemplatecompanion.php');
 
 class plgSystemLessTemplateCompanionTest extends TestCase
 {
-    public function testSetLessVariables()
+    public function testsetLessVariables()
     {
-        $test_array = array(
-	        'ltc_color_main' => '#123456',
-	        'color_link' => '#654321'
-        );
-        
-        $result_array = 'SetLessVariables';
-        $this->assertThat($result_array, $this->EqualTo(array('color_main' => '#123456')));
+	    $test_array = array(
+			'ltc_color_main' => '#123456',
+			'color_link' => '#654321'
+		);
+
+		$result_array = setLessVariables($test_array);
+
+		$this->assertThat($result_array, $this->EqualTo(array('color_main' => '#123456')));
     }
 }
 ?>
