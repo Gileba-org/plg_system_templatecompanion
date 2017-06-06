14dc186e20c3499e6de869a6e2e4ecaac0fbac7e
Gileba <Gileba@users.noreply.github.com>
2017-06-06 13:16:09 +0200

    First try

---
+++ tests/lesstemplatecompanionTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once('./src/lesstemplatecompanion.php');
+
+class plgSystemLessTemplateCompanionTest extends TestCase
+{
+	protected $plugin;
+	
+	public function setup() {
+		$this->plugin = new plgSystemLessTemplateCompanion;
+	}
+	
+	public function testSetLessVariables()
+	{
+		$test_array = array(
+			'ltc_color_main' => '#123456',
+			'color_link' => '#654321'
+		);
+
+		$result_array = $this->plugin->setLessVariables($test_array);
+
+		$this->assertThat($result_array, $this->EqualTo(array('color_main' => '#123457')));
+	}
+}
+?>
