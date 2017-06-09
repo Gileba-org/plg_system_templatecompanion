81380a4ae4a1ba56de97ed1d715d377a52f536b3
Gileba <Gileba@users.noreply.github.com>
2017-06-09 21:36:00 +0200

    deprecated

--- src/plgSystemLessTemplateCompanion.php
+++ src/plgSystemLessTemplateCompanion.php
@@ -43,7 +43,7 @@ class plgSystemLessTemplateCompanion extends JPlugin
 		// trigger parent constructor first so params get set
 		parent::__construct($subject, $config);
 
-		$client = $this->app->isSite() ? JPATH_SITE : JPATH_ADMINISTRATOR;
+		$client = $this->app->isClient('site') ? JPATH_SITE : JPATH_ADMINISTRATOR;
 		$this->templatePath = $client . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $this->app->getTemplate() . DIRECTORY_SEPARATOR;
 
 		$this->lessFile     = $this->templatePath . 'less/template.less';
