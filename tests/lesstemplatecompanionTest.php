9000c361ec1a443d50108f423f081af1e65b1a00
Gileba <info@gileba.be>
2017-06-24 21:10:54 +0200

    Camel Case

--- tests/lesstemplatecompanionTest.php
+++ tests/lesstemplatecompanionTest.php
@@ -30,38 +30,38 @@ class plgSystemlessTemplateCompanionTest extends TestCaseDatabase
 	// Test correct behavior of setLessVariable on an array with a single correct element
 	public function testParseVariable()
 	{
-		$test_array = array('ltc_main-color' => '#123456');
-		$result_array = $this->class->setLessVariables($test_array);
-		$this->assertEquals($result_array , array('main-color' => '#123456'));
+		$testArray = array('ltc_main-color' => '#123456');
+		$resultArray = $this->class->setLessVariables($testArray);
+		$this->assertEquals($resultArray , array('main-color' => '#123456'));
 	}
 
 	// Test correct behavior of setLessVariable on an array with a single incorrect element
 	public function testDoNotParseVariable()
 	{
-		$test_array = array('main-color' => '#123456', 'test' => 'empty');
-		$result_array = $this->class->setLessVariables($test_array);
-		$this->assertEquals($result_array , array());
+		$testArray = array('main-color' => '#123456', 'test' => 'empty');
+		$resultArray = $this->class->setLessVariables($testArray);
+		$this->assertEquals($resultArray , array());
 	}
 	
 	// Test correct behavior of setLessVariable on an array with different elements, including slashes
 	public function testQuoteSlash() {
-		$test_array = array('ltc_slash' => 'Joomla/Test');
-		$result_array = $this->class->setLessVariables($test_array);		
-		$this->assertEquals($result_array , array('slash' => '"Joomla/Test"'));
+		$testArray = array('ltc_slash' => 'Joomla/Test');
+		$resultArray = $this->class->setLessVariables($testArray);		
+		$this->assertEquals($resultArray , array('slash' => '"Joomla/Test"'));
 	}
 	
 	// Test correct behavior of setLessVariable on an array with different elements, including slashes
 	public function testQuoteEmptyValue() {
-		$test_array = array('ltc_empty' => '');
-		$result_array = $this->class->setLessVariables($test_array);		
-		$this->assertEquals($result_array , array('empty' => '""'));
+		$testArray = array('ltc_empty' => '');
+		$resultArray = $this->class->setLessVariables($testArray);		
+		$this->assertEquals($resultArray , array('empty' => '""'));
 	}
 	
 	// Test correct behavior of setLessVariable on an array with different elements
 	public function testOnlyParseValidVariables() {
-		$test_array = array('ltc_main-color' => '#123456', 'test' => 'empty', 'ltc_slash' => 'Joomla/Test', 'ltc_empty' => '');
-		$result_array = $this->class->setLessVariables($test_array);		
-		$this->assertEquals($result_array , array('main-color' => '#123456', 'slash' => '"Joomla/Test"', 'empty' => '""'));
+		$testArray = array('ltc_main-color' => '#123456', 'test' => 'empty', 'ltc_slash' => 'Joomla/Test', 'ltc_empty' => '');
+		$resultArray = $this->class->setLessVariables($testArray);		
+		$this->assertEquals($resultArray , array('main-color' => '#123456', 'slash' => '"Joomla/Test"', 'empty' => '""'));
 	}
 	
 	// Test correct behavior of onBeforeRender
