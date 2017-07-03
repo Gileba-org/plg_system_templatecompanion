eca9fe8b12e24f32a9bcf2c1790d0720b707b993
Gileba <Gileba@users.noreply.github.com>
2017-07-03 17:47:18 +0200

    Activate on install

---
+++ script.php
@@ -0,0 +1,23 @@
+<?php
+	defined('_JEXEC') or die;
+
+class PlgSystemLesstemplatecompanionInstallerScript {
+	/**
+	 * method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	public function postflight($route, $parent) 
+	{
+		if ($route === 'install') {
+			$query = $this->db->getQuery(true)
+				->update('#__extensions')
+				->set($this->db->quoteName('enabled') . ' = 1')
+				->where($this->db->quoteName('type') . ' = ' . $this->db->quote('plugin'))
+				->where($this->db->quoteName('element') . ' = ' . $this->db->quote('lesstemplatecompanion'))
+				->where($this->db->quoteName('folder') . ' = ' . $this->db->quote('system'));
+				$this->db->setQuery($query);
+				$this->db->execute();
+		}
+	}
+}
