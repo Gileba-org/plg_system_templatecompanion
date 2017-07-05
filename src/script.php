e401623b522063c823268cc350caa617edda2adc
Gijs Lamon <Gileba@users.noreply.github.com>
2017-07-05 09:46:11 +0200

    Auto activate on install (#55)
    * Activate on install
    * Repo optimalisation

---
+++ src/script.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('_JEXEC') or die;
+
+class plgSystemLessTemplateCompanionInstallerScript {
+
+	public $db              = null;
+
+	/**
+	 * method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	public function postflight($route, $parent) 
+	{
+		if ($route === 'install') {
+			$this->db      = JFactory::getDbo();
+
+			$query = $this->db->getQuery(true)
+				->update('#__extensions')
+				->set($this->db->quoteName('enabled') . ' = 1')
+				->where($this->db->quoteName('type') . ' = ' . $this->db->quote('plugin'))
+				->where($this->db->quoteName('element') . ' = ' . $this->db->quote('lesstemplatecompanion'))
+				->where($this->db->quoteName('folder') . ' = ' . $this->db->quote('system'));
+				$this->db->setQuery($query);
+				$this->db->execute();
+		}
+	}
+}
