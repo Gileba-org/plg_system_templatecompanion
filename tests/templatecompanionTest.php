c4609801d2c970c8284d8348abe9790b9068f1e2
Gijs Lamon <Gileba@users.noreply.github.com>
2018-03-14 23:21:13 +0100

    Start testing onExtensionAfterSave (#97)

--- tests/templatecompanionTest.php
+++ tests/templatecompanionTest.php
@@ -82,6 +82,40 @@ class PlgSystemTemplateCompanionTest extends TestCaseDatabase
 		$this->assertEquals($resultArray , array('main-color' => '#123456', 'slash' => '"Joomla/Test"', 'empty' => '""'));
 	}
 	
+	public function testOnExtensionAfterSaveWrongContext(){
+		$testContext = 'com_content';
+		$result = $this->class->onExtensionAfterSave($testContext, array(), false);
+		$this->assertEquals($result, 'wrong context');
+	}
+		
+	public function testOnExtensionAfterSaveNotUseLessComTemplates(){
+		$testContext = 'com_templates.style';
+		$table = (object) array('params' => '');
+		$result = $this->class->onExtensionAfterSave($testContext, $table, false);
+		$this->assertEquals($result, 'useLESS not implemented');
+	}
+		
+	public function testOnExtensionAfterSaveNotUseLessComAdvancedTemplates(){
+		$testContext = 'com_advancedtemplates.style';
+		$table = (object) array('params' => '');
+		$result = $this->class->onExtensionAfterSave($testContext, $table, false);
+		$this->assertEquals($result, 'useLESS not implemented');
+	}
+		
+	public function testOnExtensionAfterSaveNotReadableComTemplates(){
+		$testContext = 'com_templates.style';
+		$table = (object) array('params' => '{"useLESS":"true"}');
+		$result = $this->class->onExtensionAfterSave($testContext, $table, false);
+		$this->assertEquals($result, 'unreadable');
+	}
+		
+	public function testOnExtensionAfterSaveNotReadableComAdvancedTemplates(){
+		$testContext = 'com_advancedtemplates.style';
+		$table = (object) array('params' => '{"useLESS":"true"}');
+		$result = $this->class->onExtensionAfterSave($testContext, $table, false);
+		$this->assertEquals($result, 'unreadable');
+	}
+		
 	// Test correct behavior of onBeforeRender
 	public function testOnBeforeRenderWithoutWriteAccess() {
 		// Joomla standard testcase doesn't have a template set as default, so onBeforeRender cannot find the less source file
